a7f00b199f04066f5a3294d4e063b3ba8b7ff412
Chris Rogers <chris.rogers@frogslayer.com>
2019-05-24 20:32:00 -0500

    Made it so all file names would be recognized

--- PartyParrot.py
+++ PartyParrot.py
@@ -1,19 +1,27 @@
 import discord
+from discord.ext import commands
 import PartyParrotConstants
+import os
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+PATH = PartyParrotConstants.path
+TOKEN = PartyParrotConstants.token
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     try:
-        if message.content.startswith('!') and 'parrot' in message.content:
-            await message.channel.send(file=discord.File(PartyParrotConstants.path + '{}.gif'.format(message.content[1:])))
+        if message.content.startswith('!') and filter(lambda x: message.content[1:] in x, os.listdir(PATH)):
+            await message.channel.send(file=discord.File(PATH + '{}.gif'.format(message.content[1:])))
     except:
         await message.channel.send('Sorry, I don\'t know that bird.')
 
